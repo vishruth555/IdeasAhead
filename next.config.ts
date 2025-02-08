778d4bcdd726d1baf879716278361f93260371c8
vishruth <vishruth555@gmail.com>
2025-02-08 21:11:17 +0530

    configured gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/IdeasAhead' : '',
+  basePath: isProd ? '/IdeasAhead' : '',
+  output: 'export'
 };
 
 export default nextConfig;
